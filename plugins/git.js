8bfef5ea58e21eac1678d677432b21297039c719
vihaga444 <104292412+vihaga444@users.noreply.github.com>
2022-05-03 21:31:20 +0530

    ☺🎪🎴🃏

--- plugins/git.js
+++ plugins/git.js
@@ -2,7 +2,7 @@ let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
     let devil = `ㅤ
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/DEVILSER/DEVILSER/main/Media/Ammu/reduced_IMG-20211219-WA0031_2.jpg")).buffer(), devil, 'ᴡɪᴢᴀʀᴅ sᴇʀ  ʙʏ ᴀᴊᴍᴀʟ ᴀɴᴅ ᴀᴄʜᴜ\n\╔╗╔╗╔══╗╔══╗\n\║╚╝║║╔╗║╚║║╝\n\║╔╗║║╠╣║╔║║╗\n\╚╝╚╝╚╝╚╝╚══╝ \n\n\ https://github.com/Ajmal-Achu/Wizard-Ser \n\n\ © ī.am ꪶᴡɪᴢᴀʀᴅ sᴇʀꫂ⁩⁴⁰⁴⁩' , '❤️', '.❤️', '🙂', ',🙂')
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/DEVILSER/DEVILSER/main/Media/Ammu/reduced_IMG-20211219-WA0031_2.jpg")).buffer(), devil, 'ᴡɪᴢᴀʀᴅ sᴇʀ  ʙʏ ᴀᴊᴍᴀʟ ᴀɴᴅ ᴀᴄʜᴜ\n\╔╗╔╗╔══╗╔══╗\n\║╚╝║║╔╗║╚║║╝\n\║╔╗║║╠╣║╔║║╗\n\╚╝╚╝╚╝╚╝╚══╝ \n\n\ මගේ YouTube Channel  එක subscribe කරන්න\https://youtube.com/channel/UCayoBJWM9lqcP8qr_xf8X8g \n\n\ © ᴛͥʜɪᴋⷶsᴀɴͫᴀ⁴⁩' , 'Hi තීක්ෂණ❤', 'Hi තීක්ෂණ❤', '☺කොහොමද ඔයාට ᴛͥʜɪᴋⷶsᴀɴͫᴀ', '☺කොහොමද ඔයාට ᴛͥʜɪᴋⷶsᴀɴͫᴀ,')
 }
 handler.tags = ['main']
 handler.help = ['git']
